280ccdfa6f6fbb72165772767b1547f1dbbb3ec0
AbdildayevM <60742999+AbdildayevM@users.noreply.github.com>
2020-10-31 12:49:20 +0600

    Add project files.

---
+++ RK2MIR/Models/Food.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RK2MIR.Models
+{
+    public class Food
+    {
+        public int FoodID { get; set; }
+        public string Title { get; set; }
+        public int Cost { get; set; }
+    }
+}
