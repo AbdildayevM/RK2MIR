43c8c85fe9076cff1f6912321014ee25bb07b433
AbdildayevM <60742999+AbdildayevM@users.noreply.github.com>
2020-11-07 13:22:15 +0600

    Added view. Still solving problem with migration&PATH

---
+++ RK2MIR/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RK2MIR.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public int ClientID { get; set; }
+        public int FoodID { get; set; }
+        public DateTime Ordertime { get; set;}
+    }
+}
